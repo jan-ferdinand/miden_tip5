a2692a124b64129818ffdb8b1055e9aef3d36fe0
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-04-01 22:56:43 +0200

    factor out lib-code-to-be, write documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//! Contains Miden assembly of the [Tip5 permutation](https://eprint.iacr.org/2023/107.pdf).
+//! The binary runs the permutation once on statically defined input.
+
 use miden_stdlib::StdLibrary;
 use miden_vm::prove;
 use miden_vm::verify;
@@ -8,6 +11,12 @@ use miden_vm::ProgramInfo;
 use miden_vm::ProofOptions;
 use miden_vm::StackInputs;
 
+/// The [Tip5](https://eprint.iacr.org/2023/107.pdf) permutation.
+///
+/// While this is technically not a Miden library, it should be relatively easy to convert it to
+/// one.
+pub const TIP5_LIB: &str = "proc.tip5 push.5 add end begin push.3 call.tip5 end";
+
 fn main() {
     let assembler = Assembler::default()
         .with_library(&StdLibrary::default())
@@ -16,7 +25,7 @@ fn main() {
         .with_debug_mode(true);
 
     let program = assembler
-        .compile("proc.tip5 push.5 add end begin push.3 call.tip5 end")
+        .compile(TIP5_LIB)
         .map_err(|err| format!("Failed to compile program - {}", err))
         .unwrap();
 
